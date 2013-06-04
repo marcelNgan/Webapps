754ff48ef4bd6ea5e786f3757787b495b9c6a35c
Marcel <mpn10@doc.ic.ac.uk>
2013-06-04 18:22:20 +0100

    Map now has markers on it.... next thing to do is the check in button

--- MapTrial/gen/com/example/maptrial/R.java
+++ MapTrial/gen/com/example/maptrial/R.java
@@ -183,8 +183,8 @@ containing a value of this type.
     }
     public static final class id {
         public static final int action_settings=0x7f050006;
-        public static final int fragment1=0x7f050005;
         public static final int hybrid=0x7f050004;
+        public static final int map=0x7f050005;
         public static final int none=0x7f050000;
         public static final int normal=0x7f050001;
         public static final int satellite=0x7f050002;
--- MapTrial/src/com/example/maptrial/MainActivity.java
+++ MapTrial/src/com/example/maptrial/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.maptrial;
 
-import android.app.Activity;  
-import android.os.Bundle;  
-import android.support.v4.app.FragmentActivity;  
-public class MainActivity extends FragmentActivity {  
-     @Override  
-     protected void onCreate(Bundle arg0) {  
-          // TODO Auto-generated method stub  
-          super.onCreate(arg0);  
-          setContentView(R.layout.activity_main);  
-     }  
-}  
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends Activity {
+  static final LatLng LONDON = new LatLng(51.50722, -0.12750);
+  static final LatLng LONDONEYE = new LatLng(51.5033, -0.1197);
+  static final LatLng BUCKINGHAM = new LatLng(51.501, -0.142);
+  static final LatLng WINDSOR = new LatLng(51.4837, -0.6042);
+  private GoogleMap mMap;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+        .getMap();
+    addMarkers();
+
+    // Move the camera instantly to London with a zoom of 15.
+    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LONDON, 15));
+
+    // Zoom in, animating the camera.
+    mMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+  }
+  
+  private void addMarkers (){
+	  Marker mLondonEye = mMap.addMarker(new MarkerOptions()
+		.position(LONDONEYE)
+		.title("London Eye"));
+	  Marker mBuckinghamPalace = mMap.addMarker(new MarkerOptions()
+		.position(BUCKINGHAM)
+		.title("Buckingham Palace"));
+	  Marker mWindsorCastle = mMap.addMarker(new MarkerOptions()
+		.position(WINDSOR)
+		.title("Windsor Castle"));
+  }
+} 
