d3487afabd81d6e2939c37b0f734a4aec9e61162
unknown <josekalladanthyil@hotmail.com>
2013-06-05 06:21:45 +0100

    created a separate eclipse project for the login session because we cant get maps to show up on our phones

---
+++ logginSession/src/com/example/logginsession/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.logginsession;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity {
+
+	Button login;
+	Button register;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		addListenerOnButton();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void addListenerOnButton() {
+		 
+		login = (Button) findViewById(R.id.login_main);
+		register = (Button) findViewById(R.id.register_main);
+ 
+		login.setOnClickListener(new OnClickListener() {
+ 
+			@Override
+			public void onClick(View arg0) {
+ 
+			  Intent login = 
+                            new Intent(MainActivity.this, LoginActivity.class);
+			  MainActivity.this.startActivity(login);
+ 
+			}
+ 
+		});
+		register.setOnClickListener(new OnClickListener() {
+			 
+			@Override
+			public void onClick(View arg0) {
+ 
+			  Intent register = 
+                            new Intent(MainActivity.this, RegisterActivity.class);
+			  MainActivity.this.startActivity(register);
+ 
+			}
+ 
+		});
+ 
+	}
+}
