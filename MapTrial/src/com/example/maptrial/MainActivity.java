af694cb45a9dce8414962c98f10c9f4c1bde4380
Marcel <mpn10@doc.ic.ac.uk>
2013-06-09 00:21:13 +0100

    GPS added in

--- MapTrial/src/com/example/maptrial/MainActivity.java
+++ MapTrial/src/com/example/maptrial/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.maptrial;
 
 import android.app.Activity;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -17,21 +19,39 @@ public class MainActivity extends Activity {
   static final LatLng LONDONEYE = new LatLng(51.5033, -0.1197);
   static final LatLng BUCKINGHAM = new LatLng(51.501, -0.142);
   static final LatLng WINDSOR = new LatLng(51.4837, -0.6042);
+  LatLng myPos;
   private GoogleMap mMap;
-
+ 
+  
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
         .getMap();
-    addMarkers();
 
     // Move the camera instantly to London with a zoom of 15.
     mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LONDON, 15));
 
     // Zoom in, animating the camera.
     mMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+    
+    addMarkers();
+    mMap.setMyLocationEnabled(true);
+    mMap.getUiSettings().setMyLocationButtonEnabled(true);
+    LocationManager locationManager = (LocationManager) 
+    	getSystemService(LOCATION_SERVICE);
+    Criteria criteria = new Criteria();
+    String provider = locationManager.getBestProvider(criteria,true);
+    Location location = locationManager.getLastKnownLocation(provider);
+    if (location!=null) {
+    	double latitude = location.getLatitude();
+    	double longitude = location.getLongitude();
+    	myPos = new LatLng (latitude, longitude);
+    	mMap.addMarker(new MarkerOptions()
+    		.position(myPos).title("My Position"));
+    }
+    
   }
 
   @Override
@@ -51,4 +71,5 @@ public class MainActivity extends Activity {
 		.position(WINDSOR)
 		.title("Windsor Castle"));
   }
+  
 } 
