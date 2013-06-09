dd65d3e21aaca781d261d95897c0e10c2675354a
Marcel <mpn10@doc.ic.ac.uk>
2013-06-09 19:08:21 +0100

    check in works
    just need to know how accurate we have to be

--- MapTrial/src/com/example/maptrial/MainActivity.java
+++ MapTrial/src/com/example/maptrial/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.maptrial;
 import java.util.*;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
@@ -22,6 +26,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MainActivity extends Activity {
   Map<String, LatLng> poiMap = new HashMap<String, LatLng>();
   String [] poiName = new String [4];
+  final Context context = this;
   static final LatLng LONDON = new LatLng(51.50722, -0.12750);
   static final LatLng LONDONEYE = new LatLng(51.5033, -0.1197);
   static final LatLng BUCKINGHAM = new LatLng(51.501, -0.142);
@@ -65,7 +70,42 @@ public class MainActivity extends Activity {
 		
 		@Override
 		public void onClick(View v) {
-			finish();
+			boolean possCheckIn = false;
+			String location = "";
+			AlertDialog.Builder alertDialogBuilder 
+				= new AlertDialog.Builder(context);
+			alertDialogBuilder.setTitle("Check In");
+			
+			for(int i =0; i<poiName.length; ++i){
+				LatLng temp = poiMap.get(poiName[i]);
+				if ((Math.abs(myPos.latitude-temp.latitude) < 0.5) &
+						(Math.abs(myPos.longitude-temp.longitude) <0.5)){
+					possCheckIn = true;
+					location = poiName[i];
+				}
+			}
+			if (possCheckIn){
+				alertDialogBuilder
+				.setMessage("You are checked in to " + location)
+				.setCancelable(false)
+				.setNeutralButton("Okay", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.cancel();
+					}
+				});
+			} else {
+				alertDialogBuilder.setMessage("There is no achievement to check in to.")
+				.setCancelable(false)
+				.setNeutralButton("Okay", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.cancel();
+					}
+				});
+			}
+			AlertDialog alertDialog = alertDialogBuilder.create();
+			alertDialog.show();
 		}
 	});    
     
